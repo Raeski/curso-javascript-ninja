439acf3301bd5cef33124421dca9ee949b5960fa
Raeski <46768410+Raeski@users.noreply.github.com>
2020-05-15 21:47:30 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'brasileirao';
+
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Sao Paulo', 'Flamengo', 'Palmeiras', 'Corintians', 'Cruzeiro'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,7 +33,12 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition (arg1, arg2){
+    if (arg1 <=4){                                                                      
+      console.log("O time que está em " +arg1+ "º lugar é o" +arg2+ "!");}
+        else { console.log("Não temos a informação do time que está nessa posição.")
+    }
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
@@ -44,7 +50,12 @@ função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+undefined
+while (counter <=30) {
+     console.log(counter);
+    counter++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -59,7 +70,27 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     "Não temos o equivalente hexadecimal para [COR]."
 */
 // ?
-
+function convertToHex (color) {
+  var hexa;
+    switch (color) {
+        case 'black':
+            hexa = '00';
+            break;
+        case 'blue':
+             hexa = '10';
+            break;
+        case 'green':
+             hexa = '2';
+            break;
+        case 'white':
+             hexa ='3';
+            break;
+        default:
+            return 'Não possui o exadecimal para a '+color+' ';
+            
+    }
+    return 'O hexadecimal para a cor ' + color +' é ' +hexa+' .';
+}
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
